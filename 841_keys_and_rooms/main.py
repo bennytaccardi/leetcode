c9b471630bd699821d252376a94a93ac25bcde17
BennyTaccardi <benny.taccardi@gmail.com>
2026-01-16 00:11:10 +0100

    rooms and keys

--- 841_keys_and_rooms/main.py
+++ 841_keys_and_rooms/main.py
@@ -1,6 +1,22 @@
-from typing import List
+from typing import List, Set
 
 
 class Solution:
+    def DFS(self, adj_list: List[List[int]], node: int, visited: Set[int]):
+        visited.add(node)
+
+        for n in adj_list[node]:
+            if n not in visited:
+                self.DFS(adj_list, n, visited)
+
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        pass
+        adj_list: List[List[int]] = [[] for _ in range(len(rooms))]
+        for idx, keys in enumerate(rooms):
+            adj_list[idx].extend(keys)
+        visited = set()
+        self.DFS(adj_list, 0, visited)
+        return len(visited) == len(rooms)
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.canVisitAllRooms([[1,3],[3,0,1],[2],[0]]))
